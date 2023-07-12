1f4fec8a94730692e9663ebcfc892701e162ebde
MiguelDFM <135071730+MiguelDFM@users.noreply.github.com>
2023-07-12 20:37:12 +0100

    add files
    tentativa de tornar isso publico

---
+++ projeto javascript/script.js
@@ -0,0 +1,6 @@
+function alertar(){
+    alert("A dama mais linda do mundo, por ser aparecida a Amandinha")
+}
+
+const dynamicImage = document.getElementById("dynamicImage");
+dynamicImage.src = "2.png";
